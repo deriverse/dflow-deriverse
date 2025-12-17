833ebdcd75c12d0fe468ad0a374a97aba948a0dd
Bogda165 <koval.b@icloud.com>
2025-12-17 15:32:06 +0100

    Init commit

---
+++ dflow-amm-interface/src/swap.rs
@@ -0,0 +1,16 @@
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Side {
+    Bid,
+    Ask,
+}
+
+/// This enum is a dflow backend enum and does not map 1:1 to the onchain aggregator Swap enum
+#[derive(Debug, Clone, Default, PartialEq)]
+pub enum Swap {
+    #[default]
+    Placeholder,
+    Deriverse {
+        side: Side,
+        instr_id: u32,
+    },
+}
