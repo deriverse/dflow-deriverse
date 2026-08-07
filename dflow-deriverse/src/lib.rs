676f075349f1a037c26c519f697711b455ca92db
Bogda165 <koval.b@icloud.com>
2026-08-07 10:57:01 +0200

    Fixed get_reversed_amm_sum

--- dflow-deriverse/src/lib.rs
+++ dflow-deriverse/src/lib.rs
@@ -527,6 +527,10 @@ impl Amm for Deriverse {
                 }
             }
 
+            if remaining_sum < 0 {
+                bail!("Match engine failed")
+            }
+
             client_tokens = client_tokens.checked_add_capped(qty)?;
 
             if remaining_sum == 1 {
@@ -716,6 +720,11 @@ impl Amm for Deriverse {
 
                 break;
             }
+
+            if remaining_qty < 0 {
+                bail!("Match engine failed")
+            }
+
             client_tokens = client_tokens.checked_sub_capped(input_amount.sub(remaining_qty))?;
             client_mints = client_mints.checked_add_capped(sum)?;
 
